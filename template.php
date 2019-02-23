52fde0b68d375964a2788d61a25762e22d58a737
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2019-02-23 09:57:51 -0500

    Add option to header for additional html

--- template.php
+++ template.php
@@ -7,7 +7,7 @@ function htmlescape($string)
 	return htmlspecialchars($string, ENT_QUOTES | ENT_HTML5);
 }
 
-function template_header($title = null, $header = true)
+function template_header($title = null, $header = true, $additional_header_html = '')
 {
 	echo '<!DOCTYPE html>'.PHP_EOL;
 	echo '<html lang="en">';
@@ -18,10 +18,11 @@ function template_header($title = null, $header = true)
 	echo '<link rel="stylesheet" href="style.css" type="text/css" />';
 	echo '<script src="jquery.js"></script>';
 	echo '<script src="script.js"></script>';
+	echo $additional_header_html;
 	echo '</head>';
 	echo '<body>';
 	if ($header) {
-		echo '<nav>Welcome, ' . htmlescape($_SESSION['user_name']) . ' | <a href="results.php">Results</a> | <a href="scores_overview.php">Scores Overview</a> | <a href="teams.php">Manage Clubs/Teams</a> | <a href="events.php">Manage Events</a> | <a href="users.php">Manage Users</a> | <a href="import_export.php">Import/Export Data</a> | <a href="logout.php">Log Out</a></nav>';
+		echo '<nav>Welcome, ' . htmlescape($_SESSION['user_name']) . ' | <a href="results.php">Results</a> | <a href="scores.php">Score Entry</a> | <a href="scores_overview.php">Scores Overview</a> | <a href="teams.php">Manage Clubs/Teams</a> | <a href="events.php">Manage Events</a> | <a href="users.php">Manage Users</a> | <a href="import_export.php">Import/Export Data</a> | <a href="logout.php">Log Out</a></nav>';
 		echo '<header><h1>' . (!is_null($title) ? htmlescape($title) . ' - ' : '') . htmlescape(APPLICATION_NAME) . '</h1></header>';
 	}
 	echo '<noscript><strong class="error" style="font-size: 200%; text-decoration: underline;">*** WARNING: You need to have Javascript enabled to use all features of this application correctly. ***</strong></noscript>';
