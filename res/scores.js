84ecb04624a6acafd73f0c51b4aa5244604429c6
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2019-05-01 22:06:13 -0400

    Work on score entry

--- res/scores.js
+++ res/scores.js
@@ -1,5 +1,9 @@
+"use strict"
+
+var score_type = null;
+
 $(function () {
-	$("#score_form").submit(function(e) {
+	$("#score_form").submit(function (e) {
 		e.preventDefault();
 		setScore();
 	});
@@ -18,12 +22,37 @@ $(function () {
 
 	$("#event").change(function () {
 		$("#status").text("");
+		score_type = $('#event').find(":selected").length > 0 ? $('#event').find(":selected").data("event_type") : null;
+		switch (score_type) {
+			case "points":
+				$("#team").prop("disabled", $("#club").val() == null);
+				$("#score_points").show();
+				$("#score_time").hide();
+				$("#score_errors").hide();
+				break;
+			case "timed":
+				$("#team").prop("disabled", $("#club").val() == null);
+				$("#score_points").hide();
+				$("#score_time").show();
+				$("#score_errors").show();
+				break;
+			case "individual":
+				$("#team").prop("disabled", true).val("");
+				$("#score_points").hide();
+				$("#score_time").hide();
+				$("#score_errors").hide();
+				break;
+		}
+		getTeams();
 		getScore();
 	});
 
 	$("#club").change(function () {
 		$("#status").text("");
 		getTeams();
+		if (score_type == "individual") {
+			getScore();
+		}
 	});
 
 	$("#team").change(function () {
@@ -31,7 +60,17 @@ $(function () {
 		getScore();
 	});
 
-	$("#score").keydown(function () {
+	$("#score_points_value").keydown(function () {
+		$("#status").text("");
+	}).change(function () {
+		$("#status").text("");
+	});
+	$("#score_time_value").keydown(function () {
+		$("#status").text("");
+	}).change(function () {
+		$("#status").text("");
+	});
+	$("#score_errors_value").keydown(function () {
 		$("#status").text("");
 	}).change(function () {
 		$("#status").text("");
@@ -42,14 +81,16 @@ function getCompetitions() {
 	$("#competition").prop("disabled", true).find("option").remove().end().append("<option value=\"\" selected=\"selected\" disabled=\"disabled\">-- Select Competition --</option>").val("");
 	$("#event").prop("disabled", true).find("option").remove().end().append("<option value=\"\" selected=\"selected\" disabled=\"disabled\">-- Select Event --</option>").val("");
 	$("#team").prop("disabled", true).find("option").remove().end().append("<option value=\"\" selected=\"selected\" disabled=\"disabled\">-- Select Team --</option>").val("");
-	$("#score").prop("disabled", true).val("");
+	$("#score_points_value").prop("disabled", true).val("");
+	$("#score_time_value").prop("disabled", true).val("");
+	$("#score_errors_value").prop("disabled", true).val("");
 	$("#submit").prop("disabled", true);
+	score_type = null;
 
 	if ($("#year").val() !== null) {
 		$.post(
-			"scores.php",
+			"scores.php?action=get_competitions",
 			{
-				action: "get_competitions",
 				year_id: $("#year").val(),
 				_csrf_token: $("#csrf_token").val()
 			},
@@ -68,20 +109,22 @@ function getCompetitions() {
 
 function getEvents() {
 	$("#event").prop("disabled", true).find("option").remove().end().append("<option value=\"\" selected=\"selected\" disabled=\"disabled\">-- Select Event --</option>").val("");
-	$("#score").prop("disabled", true).val("");
+	$("#score_points_value").prop("disabled", true).val("");
+	$("#score_time_value").prop("disabled", true).val("");
+	$("#score_errors_value").prop("disabled", true).val("");
 	$("#submit").prop("disabled", true);
+	score_type = null;
 
 	if ($("#competition").val() !== null) {
 		$.post(
-			"scores.php",
+			"scores.php?action=get_events",
 			{
-				action: "get_events",
 				competition_id: $("#competition").val(),
 				_csrf_token: $("#csrf_token").val()
 			},
 			function (events) {
 				events.forEach(function (event) {
-					$("#event").append($("<option>").val(event.id).text(event.name));
+					$("#event").append($("<option>").val(event.id).text(event.name).data("event_type", event.type));
 				});
 				$("#event").prop("disabled", false);
 			},
@@ -95,14 +138,15 @@ function getEvents() {
 function getClubs() {
 	$("#club").prop("disabled", true).find("option").remove().end().append("<option value=\"\" selected=\"selected\" disabled=\"disabled\">-- Select Club --</option>").val("");
 	$("#team").prop("disabled", true).find("option").remove().end().append("<option value=\"\" selected=\"selected\" disabled=\"disabled\">-- Select Team --</option>").val("");
-	$("#score").prop("disabled", true).val("");
+	$("#score_points_value").prop("disabled", true).val("");
+	$("#score_time_value").prop("disabled", true).val("");
+	$("#score_errors_value").prop("disabled", true).val("");
 	$("#submit").prop("disabled", true);
 
 	if ($("#year").val() !== null) {
 		$.post(
-			"scores.php",
+			"scores.php?action=get_clubs",
 			{
-				action: "get_clubs",
 				year_id: $("#year").val(),
 				_csrf_token: $("#csrf_token").val()
 			},
@@ -121,14 +165,15 @@ function getClubs() {
 
 function getTeams() {
 	$("#team").prop("disabled", true).find("option").remove().end().append("<option value=\"\" selected=\"selected\" disabled=\"disabled\">-- Select Team --</option>").val("");
-	$("#score").prop("disabled", true).val("");
+	$("#score_points_value").prop("disabled", true).val("");
+	$("#score_time_value").prop("disabled", true).val("");
+	$("#score_errors_value").prop("disabled", true).val("");
 	$("#submit").prop("disabled", true);
 
-	if ($("#club").val() !== null && $("#competition").val() !== null) {
+	if ($("#club").val() !== null && $("#competition").val() !== null && score_type != "individual") {
 		$.post(
-			"scores.php",
+			"scores.php?action=get_teams",
 			{
-				action: "get_teams",
 				club_id: $("#club").val(),
 				competition_id: $("#competition").val(),
 				_csrf_token: $("#csrf_token").val()
@@ -137,7 +182,9 @@ function getTeams() {
 				teams.forEach(function (team) {
 					$("#team").append($("<option>").val(team.id).text(team.name));
 				});
-				$("#team").prop("disabled", false);
+				if (score_type != "individual") {
+					$("#team").prop("disabled", false);
+				}
 			},
 			"json"
 		).fail(function () {
@@ -147,45 +194,131 @@ function getTeams() {
 }
 
 function getScore() {
-	$("#score").prop("disabled", true).val("");
+	$("#score_points_value").prop("disabled", true).val("");
+	$("#score_time_value").prop("disabled", true).val("");
+	$("#score_errors_value").prop("disabled", true).val("");
 	$("#submit").prop("disabled", true);
 
-	if ($("#event").val() !== null && $("#team").val() !== null) {
-		$.post(
-			"scores.php",
-			{
-				action: "get_score",
-				team_id: $("#team").val(),
-				event_id: $("#event").val(),
-				_csrf_token: $("#csrf_token").val()
-			},
-			function (response) {
-				$("#score").val(response.score).prop("disabled", false);
-				$("#submit").prop("disabled", false);
-			},
-			"text"
-		).fail(function () {
-			alert("An error occurred getting the score.");
-		});
+	switch (score_type) {
+		case "points":
+			if ($("#event").val() !== null && $("#team").val() !== null) {
+				$.post(
+					"scores.php?action=get_score",
+					{
+						team_id: $("#team").val(),
+						event_id: $("#event").val(),
+						_csrf_token: $("#csrf_token").val()
+					},
+					function (response) {
+						$("#score_points_value").prop("disabled", false);
+						if (response.points !== null) {
+							$("#score_points_value").val(response.points);
+						} else {
+							$("#score_points_value").val("");
+						}
+						$("#submit").prop("disabled", false);
+					},
+					"text"
+				).fail(function () {
+					alert("An error occurred getting the score.");
+				});
+			}
+			break;
+		case "timed":
+			if ($("#event").val() !== null && $("#team").val() !== null) {
+				$.post(
+					"scores.php?action=get_score",
+					{
+						team_id: $("#team").val(),
+						event_id: $("#event").val(),
+						_csrf_token: $("#csrf_token").val()
+					},
+					function (response) {
+						$("#score_time_value").prop("disabled", false);
+						$("#score_errors_value").prop("disabled", false);
+						if (response.time !== null) {
+							$("#score_time_value").val(response.time);
+						} else {
+							$("#score_time_value").val("");
+						}
+						if (response.errors !== null) {
+							$("#score_errors_value").val(response.errors);
+						} else {
+							$("#score_errors_value").val("");
+						}
+						$("#submit").prop("disabled", false);
+					},
+					"text"
+				).fail(function () {
+					alert("An error occurred getting the score.");
+				});
+			}
+			break;
+		case "individual":
+			if ($("#event").val() !== null && $("#club").val() !== null) {
+				$.post(
+					"scores.php?action=get_score",
+					{
+						club_id: $("#club").val(),
+						event_id: $("#event").val(),
+						_csrf_token: $("#csrf_token").val()
+					},
+					function (response) {
+						
+						$("#submit").prop("disabled", false);
+					},
+					"text"
+				).fail(function () {
+					alert("An error occurred getting the score.");
+				});
+			}
+			break;
 	}
 }
 
 function setScore() {
-	if ($("#event").val() !== null && $("#team").val() !== null) {
-		$.post(
-			"scores.php",
-			{
-				action: "set_score",
-				team_id: $("#team").val(),
-				event_id: $("#event").val(),
-				score: $("#score").val(),
-				_csrf_token: $("#csrf_token").val()
-			},
-			function () {
-				$("#status").text("Saved");
+	switch (score_type) {
+		case "points":
+			if ($("#event").val() !== null && $("#team").val() !== null) {
+				$.post(
+					"scores.php?action=set_score",
+					{
+						team_id: $("#team").val(),
+						event_id: $("#event").val(),
+						points: $("#score_points_value").val(),
+						_csrf_token: $("#csrf_token").val()
+					},
+					function () {
+						$("#status").text("Saved");
+					},
+					"text"
+				).fail(function () {
+					alert("An error occurred setting the score.");
+				});
 			}
-		).fail(function () {
-			alert("An error occurred setting the score.");
-		});
+			break;
+		case "timed":
+			if ($("#event").val() !== null && $("#team").val() !== null) {
+				$.post(
+					"scores.php?action=set_score",
+					{
+						team_id: $("#team").val(),
+						event_id: $("#event").val(),
+						time: $("#score_time_value").val(),
+						errors: $("#score_errors_value").val(),
+						_csrf_token: $("#csrf_token").val()
+					},
+					function () {
+						$("#status").text("Saved");
+					},
+					"text"
+				).fail(function () {
+					alert("An error occurred setting the score.");
+				});
+			}
+			break;
+		case "individual":
+
+			break;
 	}
 }
--- scores.php
+++ scores.php
@@ -6,8 +6,9 @@ require_once('auth.php');
 
 require_once('database.php');
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['action'], $_POST['_csrf_token']) && $_POST['_csrf_token'] === $_SESSION['csrf_token']) {
-	switch ($_POST['action']) {
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_GET['action'], $_POST['_csrf_token']) && $_POST['_csrf_token'] === $_SESSION['csrf_token']) {
+	header('Content-Type: application/json');
+	switch ($_GET['action']) {
 		case 'get_competitions':
 			if (isset($_POST['year_id'])) {
 				$competitions = [];
@@ -53,33 +54,85 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['action'], $_POST['_csr
 			}
 			break;
 		case 'get_score':
-			if (isset($_POST['team_id'], $_POST['event_id'])) {
-				/*$score = database_query('SELECT "points" FROM "scores" WHERE "team" = ? AND "event" = ?;', [(int)$_POST['team_id'], (int)$_POST['event_id']]);
-				if (isset($score[0])) {
-					echo round($score[0]['points'], 2);
+			if (isset($_POST['event_id'])) {
+				$event_details = database_query('SELECT "type" FROM "events" WHERE "id" = ?', [(int)$_POST['event_id']]);
+				if (isset($event_details[0])) {
+					$event_type = $event_details[0]['type'];
+					switch ($event_type) {
+						case 'points':
+							if (isset($_POST['team_id'])) {
+								$score = database_query('SELECT "points" FROM "point_scores" WHERE "team" = ? AND "event" = ?', [(int)$_POST['team_id'], (int)$_POST['event_id']]);
+								if (isset($score[0])) {
+									echo json_encode(['type' => 'points', 'points' => (float)$score['points']]);
+								} else {
+									echo json_encode(['type' => 'points', 'points' => null]);
+								}
+							} else {
+								http_response_code(400);
+							}
+							break;
+						case 'timed':
+							if (isset($_POST['team_id'])) {
+								$score = database_query('SELECT "time", "errors" FROM "timed_scores" WHERE "team" = ? AND "event" = ?', [(int)$_POST['team_id'], (int)$_POST['event_id']]);
+								if (isset($score[0])) {
+									echo json_encode(['type' => 'timed', 'time' => (float)$score['time'], 'errors' => (float)$score['errors']]);
+								} else {
+									echo json_encode(['type' => 'timed', 'time' => null, 'errors' => 0.0]);
+								}
+							} else {
+								http_response_code(400);
+							}
+							break;
+						case 'individual':
+							if (isset($_POST['club_id'])) {
+								$results = database_query('SELECT "id", "name", "points" FROM "individual_scores" WHERE "club" = ? AND "event" = ?', [(int)$_POST['club_id'], (int)$_POST['event_id']]);
+								$scores = [];
+								foreach ($results as $result) {
+									$scores[] = ['id' => $result['id'], 'name' => $result['name'], 'points' => (float)$result['points']];
+								}
+								echo json_encode(['type' => 'individual', 'scores' => $scores]);
+							} else {
+								http_response_code(400);
+							}
+							break;
+						default:
+							http_response_code(500);
+							break;
+					}
 				} else {
-					echo '';
-				}*/
-			} else {
-				http_response_code(400);
-			}
-			break;
-		case 'set_score':
-			if (isset($_POST['team_id'], $_POST['event_id'], $_POST['score'])) {
-				// check if there is a score to insert
-				if ($_POST['score'] !== '') {
-					// input the score
-					
-				} else {
-					// clear the score
-					
+					http_response_code(404);
 				}
 			} else {
 				http_response_code(400);
 			}
 			break;
+		case 'set_score':
+			if (isset($_POST['event_id'])) {
+				$event_details = database_query('SELECT "type" FROM "events" WHERE "id" = ?', [(int)$_POST['event_id']]);
+				if (isset($event_details[0])) {
+					$event_type = $event_details[0]['type'];
+					switch ($event_type) {
+						case 'points':
+
+							break;
+						case 'timed':
+
+							break;
+						case 'individual':
+
+							break;
+						default:
+							http_response_code(500);
+							break;
+					}
+				} else {
+					http_response_code(400);
+				}
+			} else {
+				http_response_code(404);
+			}
+			break;
 		default:
-			$_SESSION['scores_error'] = 'An error occurred.';
 			http_response_code(400);
 			break;
 	}
@@ -90,13 +143,6 @@ require_once('template.php');
 
 template_header('Score Entry', true, '<script src="res/scores.js"></script>');
 
-if (isset($_SESSION['scores_error'])) {
-	echo '<script>alert(';
-	echo json_encode($_SESSION['scores_error']);
-	echo ');</script>';
-	unset($_SESSION['scores_error']);
-}
-
 echo '<form id="score_form" action="scores.php" method="post" class="form">';
 
 echo '<input id="csrf_token" name="_csrf_token" value="' . htmlescape($_SESSION['csrf_token']) . '" type="hidden" />';
@@ -105,82 +151,113 @@ $years = database_query('SELECT "id", "name" FROM "years" ORDER BY "name";');
 usort($years, function ($a, $b) {
 	return strnatcasecmp($a['name'], $b['name']);
 });
-echo '<div class="form-element">';
-echo '<div class="form-subelement">';
+
+echo '<table>';
+
+echo '<tbody>';
+
+echo '<tr>';
+echo '<td>';
 echo '<label for="year">Year:</label>';
-echo '</div>';
-echo '<div class="form-subelement">';
+echo '</td>';
+echo '<td>';
 echo '<select id="year" autofocus="autofocus">';
 echo '<option value="" selected="selected" disabled="disabled">-- Select Year --</option>';
 foreach ($years as $year) {
 	echo '<option value="' . htmlescape($year['id']) . '">' . htmlescape($year['name']) . '</option>';
 }
 echo '</select>';
-echo '</div>';
-echo '</div>';
+echo '</td>';
+echo '</tr>';
 
-echo '<div class="form-element">';
-echo '<div class="form-subelement">';
+echo '<tr>';
+echo '<td>';
 echo '<label for="competition">Competition:</label>';
-echo '</div>';
-echo '<div class="form-subelement">';
+echo '</td>';
+echo '<td>';
 echo '<select id="competition" disabled="disabled">';
 echo '<option value="" selected="selected" disabled="disabled">-- Select Competition --</option>';
 echo '</select>';
-echo '</div>';
-echo '</div>';
+echo '</td>';
+echo '</tr>';
 
-echo '<div class="form-element">';
-echo '<div class="form-subelement">';
+echo '<tr>';
+echo '<td>';
 echo '<label for="event">Event:</label>';
-echo '</div>';
-echo '<div class="form-subelement">';
+echo '</td>';
+echo '<td>';
 echo '<select id="event" disabled="disabled">';
 echo '<option value="" selected="selected" disabled="disabled">-- Select Event --</option>';
 echo '</select>';
-echo '</div>';
-echo '</div>';
+echo '</td>';
+echo '</tr>';
 
-echo '<div class="form-element">';
-echo '<div class="form-subelement">';
+echo '<tr>';
+echo '<td>';
 echo '<label for="club">Club:</label>';
-echo '</div>';
-echo '<div class="form-subelement">';
+echo '</td>';
+echo '<td>';
 echo '<select id="club" disabled="disabled">';
 echo '<option value="" selected="selected" disabled="disabled">-- Select Club --</option>';
 echo '</select>';
-echo '</div>';
-echo '</div>';
+echo '</td>';
+echo '</tr>';
 
-
-echo '<div class="form-element">';
-echo '<div class="form-subelement">';
+echo '<tr>';
+echo '<td>';
 echo '<label for="team">Team:</label>';
-echo '</div>';
-echo '<div class="form-subelement">';
+echo '</td>';
+echo '<td>';
 echo '<select id="team" disabled="disabled">';
 echo '<option value="" selected="selected" disabled="disabled">-- Select Team --</option>';
 echo '</select>';
-echo '</div>';
-echo '</div>';
+echo '</td>';
+echo '</tr>';
 
+echo '<tr id="score_points">';
+echo '<td>';
+echo '<label for="score_points_value">Points:</label>';
+echo '</td>';
+echo '<td>';
+echo '<input id="score_points_value" value="" type="number" min="0" step="0.01" disabled="disabled" />';
+echo '</td>';
+echo '</tr>';
 
-echo '<div class="form-element">';
-echo '<div class="form-subelement">';
-echo '<label for="score">Score:</label>';
-echo '</div>';
-echo '<div class="form-subelement">';
-echo '<input id="score" value="" type="number" min="0" step="0.01" disabled="disabled" />';
-echo '</div>';
-echo '</div>';
+echo '<tr id="score_time" style="display: none;">';
+echo '<td>';
+echo '<label for="score_time_value">Time:</label>';
+echo '</td>';
+echo '<td>';
+echo '<input id="score_time_value" value="" type="text" maxlength="250" disabled="disabled" />';
+echo '</td>';
+echo '</tr>';
 
-echo '<div class="form-element">';
-echo '<div class="form-subelement"></div>';
-echo '<div class="form-subelement">';
+echo '<tr id="score_errors" style="display: none;">';
+echo '<td>';
+echo '<label for="score_errors_value">Errors:</label>';
+echo '</td>';
+echo '<td>';
+echo '<input id="score_errors_value" value="" type="number" min="0" step="0.01" disabled="disabled" />';
+echo '</td>';
+echo '</tr>';
+
+echo '</tbody>';
+
+echo '<tbody id="individual_scores"></tbody>';
+
+echo '<tfoot>';
+
+echo '<tr>';
+echo '<td></td>';
+echo '<td>';
 echo '<input id="submit" type="submit" value="Save" disabled="disabled" />';
-echo ' <span id="status"></span>';
-echo '</div>';
-echo '</div>';
+echo '&nbsp;<span id="status"></span>';
+echo '</td>';
+echo '</tr>';
+
+echo '</tfoot>';
+
+echo '</table>';
 
 echo '</form>';
 
